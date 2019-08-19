594afd72194c8fdedc6a249c144e6c51f73ea34f
hmmhmmhm <hmmhmmhm@naver.com>
2019-08-19 14:52:17 +0900

    Add

--- src/script.ts
+++ src/script.ts
@@ -16,28 +16,6 @@ const { Rule, Player, Event, Entity, Team, Vector } = Classes
  * Write the workshop code from below.
  */
 
-console.log("script.ts say hello?")
-
-
-// Check operator overload
-let a = Value.number(1)
-let b = Value.number(2)
-let c = Value.number(3)
-let d = Value.number(4)
-let e = Value.number(4)
-let f = Value.number(4)
-let g = Value.number(4)
-let h = Value.number(4)
-let i = Value.number(4)
-let j = Value.number(4)
-let k = Value.number(4)
-let l = Value.number(4)
-let m = Value.number(4)
-let n = Value.number(4)
-let o = Value.number(4)
-
-let checkOperator1 = ((a*b)/c+d) % e + (f+g+h+i) - (j-(k*l/m) - n * o)
-
 // Rule Template
 new Rule({
 
